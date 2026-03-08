b996391a22b018581499be065823c6e9c3a7b4d0
dodistriandka <andikadodisatria@gmail.com>
2026-03-08 12:10:33 +0700

    'database'

--- database/migrations/2026_02_09_081958_create_tempat_sampahs_table.php
+++ database/migrations/2026_02_09_081958_create_tempat_sampahs_table.php
@@ -13,11 +13,28 @@ return new class extends Migration
 {
     Schema::create('tempat_sampahs', function (Blueprint $table) {
         $table->id();
+
+        // IDENTITAS TEMPAT SAMPAH
         $table->string('kode_tempat')->unique(); // TS001
         $table->string('lokasi');
+
+        // TINGGI TEMPAT SAMPAH
         $table->integer('kapasitas_cm');
+
+        // DATA SENSOR ESP32
+        $table->float('jarak_sensor')->nullable();
+        $table->boolean('pir')->default(0);
+        $table->boolean('relay_suara')->default(0);
+
+        // HASIL PERHITUNGAN SISTEM
+        $table->integer('persentase')->default(0);
+
         $table->enum('status', ['kosong', 'hampir_penuh', 'penuh'])
               ->default('kosong');
+
+        // WAKTU UPDATE SENSOR
+        $table->timestamp('last_sensor_update')->nullable();
+
         $table->timestamps();
     });
 }
